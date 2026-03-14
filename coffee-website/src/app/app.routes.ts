a996772632bf1c794f72c5d1dacf9224f363053b
james-e-crandall <james.e.crandall@gmail.com>
2026-03-13 20:10:21 -0500

    add home-page

--- coffee-website/src/app/app.routes.ts
+++ coffee-website/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home-page/home-page';
 
-export const routes: Routes = [];
+export const HomePageRoute = {
+  path: '',
+  component: HomePage,
+};
+
+// Redirect any other URLs that don’t match
+// (also known as a "wildcard" redirect)
+export const RedirectRoute = {
+  path: '**',
+  redirectTo: '/',
+};
+
+export const routes: Routes = [
+  HomePageRoute,
+  RedirectRoute
+];
